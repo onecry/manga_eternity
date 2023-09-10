05cbd9d3f8bb13b5ecfffb1564bdefc19d69056f
onecry <onecryyyyy@gmail.com>
2023-09-11 02:14:07 +0700

    Manga details added

--- mangaeternity/search/urls.py
+++ mangaeternity/search/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_manga_name_view, get_titles_view
+from .views import get_manga_name_view, get_titles_view, title_details_view
 
 app_name = "search"
 
 urlpatterns = [
     path('search/', get_manga_name_view, name='manga_search'),
     path('search/titles/', get_titles_view, name='titles_list'),
+    path('seatch/titles/<manga_id>/', title_details_view, name='title_details')
 ]
--- mangaeternity/search/views.py
+++ mangaeternity/search/views.py
@@ -1,7 +1,7 @@
 import requests
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
-from django.views.generic import View, ListView, TemplateView
+from django.views.generic import View, ListView, TemplateView, DetailView
 
 from mangaeternity.settings import MANGA_URL
 from .forms import MangaTitleForm
@@ -20,6 +20,7 @@ def get_manga_name_view(request:HttpRequest):
         form = MangaTitleForm()
         return render(request, template_name, {"form": form})
     
+    
 def get_titles_view(request:HttpRequest):
     template_name = "search/titles_list.html"
     title = request.session.get('title')
@@ -29,3 +30,17 @@ def get_titles_view(request:HttpRequest):
     manga_data = mangadex_r["results"]
         
     return render(request, template_name, {"manga_data": manga_data})
+
+
+def title_details_view(request:HttpRequest, manga_id):
+    template_name = "search/title_details.html"
+    
+    mangadex_r = requests.get(f"{MANGA_URL}info/{manga_id}").json()
+    
+    #did this because sometimes description ends with '---' or '**' and after thah symbols was manga awards or links to author
+    manga_description = mangadex_r["description"]["en"].replace('---', '**').split('**')[0]
+    
+    return render(request, template_name, {"manga_data": mangadex_r, 'manga_description': manga_description})
+
+def manga_chapters_view(request:HttpRequest, manga_id):
+    pass
